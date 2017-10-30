7dc505510a622e8b265dab3e102bde0f3b4b3b72
Breinstew <snowway@list.ru>
2017-10-30 12:29:08 +0300

    Проверка Gulp, Grant, node

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+// Зависимости Gulp будут здесь
+gulp.task('default', function() {
+    // Задачи Gulp будут здесь
+    // Исходный код для Node
+    gulp.src("es6/**/*.js")
+       .pipe(babel())
+       .pipe(gulp.dest("dist"));
+    // Исходный код браузера
+    gulp.src("public/es6/**/*.js")
+       .pipe(babel())
+       .pipe(gulp.dest("public/dist"));
+});
